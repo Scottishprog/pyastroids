1ecee6611d5eccfabd224420b867a4a33159ef68
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-01-06 14:58:05 -0800

    Started work on display and main loop

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Main Game Loop
+    
 
 if __name__ == "__main__":
     main()

e1f5a08343b441d4b8e3411547254b7cb20384da
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-01-06 15:11:55 -0800

    Finished initial window setup.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import Color
+
 from constants import *
 
 def main():
@@ -6,10 +8,15 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
-    pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
     #Main Game Loop
-    
+    while(True):
+        screen.fill(color=Color(0,0,0), rect=None)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()

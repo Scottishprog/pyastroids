905d00e8292c916f63a7d91d998dfc1f96940b0b
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2025-01-09 15:18:57 -0800

    Added rotation control: a=left, d=right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from pygame import Color
 
 from constants import *
@@ -9,14 +10,28 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    deltaClock = pygame.time.Clock()
+    dt = 0
+
+    # Init Player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     #Main Game Loop
     while(True):
-        screen.fill(color=Color(0,0,0), rect=None)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+        screen.fill("black", rect=None)
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = deltaClock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
